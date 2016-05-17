38ca0c86111015c133fd031a8ef5eb16ccd136f7
Shawn Gilroy <shawnpgilroy@gmail.com>
2016-05-17 09:59:31 -0400

    Update InformationWindow.xaml.cs

--- small_n_stats_WPF/small_n_stats_WPF/Views/InformationWindow.xaml.cs
+++ small_n_stats_WPF/small_n_stats_WPF/Views/InformationWindow.xaml.cs
@@ -1,9 +1,21 @@
-﻿/*
- * Shawn Gilroy, Copyright 2016. Licensed under GPL-2.
- * Small n Stats Application
- * Modeled from conceptual work developed by Richard Parker (non-parametric statistics in time series)
- * 
- */
+/* 
+    Copyright 2016 Shawn Gilroy
+
+    This file is part of Demand Analysis.
+
+    Demand Analysis is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 2.
+
+    Demand Analysis is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Demand Analysis.  If not, see <http://www.gnu.org/licenses/gpl-2.0.html>.
+
+*/
 
 using System.Diagnostics;
 using System.Windows;
